59008f38d751874a386df51770331931c38de9f2
Xsidelight <toko.gogberaa@gmail.com>
2023-12-16 01:00:49 +0600

    Add health check and optimised code

--- lb/main.go
+++ lb/main.go
@@ -5,12 +5,15 @@ import (
 	"io"
 	"net"
 	"net/http"
+	"sync"
 	"sync/atomic"
+	"time"
 )
 
 var (
 	backendServers = []string{"127.0.0.1:8080", "127.0.0.1:8081", "127.0.0.1:8082"}
 	currentServer  uint64
+	healthChecks   sync.Once
 )
 
 func getNextBackendServer() string {
@@ -19,12 +22,24 @@ func getNextBackendServer() string {
 }
 
 func healthCheck(backendAddr string) {
-	response, err := http.Get("http://" + backendAddr)
-	if err != nil || response.StatusCode != 200 {
-		fmt.Printf("Server is unavailable: %s\n", err)
-		return
-	}
+	ticker := time.NewTicker(10 * time.Second)
+	defer ticker.Stop()
 
+	for range ticker.C {
+		resp, err := http.Get("http://" + backendAddr)
+		if err != nil {
+			fmt.Printf("Server %s is unavailable: %s\n", backendAddr, err)
+			continue
+		}
+		fmt.Printf("Server %s is healthy\n", backendAddr)
+		resp.Body.Close()
+	}
+}
+
+func startHealthChecks() {
+	for _, addr := range backendServers {
+		go healthCheck(addr)
+	}
 }
 
 func handleConnection(conn net.Conn) {
@@ -32,33 +47,33 @@ func handleConnection(conn net.Conn) {
 
 	backendAddr := getNextBackendServer()
 
-	// Connect to backend server
 	backendConn, err := net.Dial("tcp", backendAddr)
 	if err != nil {
 		fmt.Printf("Error connecting to backend: %s\n", err)
 		return
 	}
-
 	defer backendConn.Close()
 
-	// Forward request to backend
-	go io.Copy(backendConn, conn)
-
-	// Forward response to client
-	io.Copy(conn, backendConn)
-
-	fmt.Println("Response from server sent back to client")
+	// Use io.Copy for bi-directional copying
+	if _, err := io.Copy(backendConn, conn); err != nil {
+		fmt.Println("Error forwarding request:", err)
+		return
+	}
+	if _, err := io.Copy(conn, backendConn); err != nil {
+		fmt.Println("Error sending response:", err)
+	}
 }
 
 func main() {
-	lbAddr := ":80"
+	// Start health checks once for all servers
+	healthChecks.Do(startHealthChecks)
 
+	lbAddr := ":80"
 	listener, err := net.Listen("tcp", lbAddr)
 	if err != nil {
 		fmt.Printf("Error listening on %s: %s\n", lbAddr, err)
 		return
 	}
-
 	defer listener.Close()
 
 	fmt.Printf("Load Balancer listening on %s\n", lbAddr)
@@ -67,11 +82,9 @@ func main() {
 		conn, err := listener.Accept()
 		if err != nil {
 			fmt.Printf("Error accepting connection: %s\n", err)
-			return
+			continue
 		}
 
-		fmt.Printf("Connection accepted from %s\n", conn.RemoteAddr())
 		go handleConnection(conn)
 	}
-
 }
